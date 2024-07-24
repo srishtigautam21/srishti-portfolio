e8d9069df9c976f3b03c3e6b3f10ff20b6dff99f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-07-24 20:22:32 +0530

    Added image using nextjs Image comp

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,29 +1,44 @@
+"use client";
+
+import profile1 from "../../src/asset/profile-photo-one.jpg";
+import Image from "next/image";
+
 function About() {
   return (
-    <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-start justify-center relative overflow-hidden py-10 px-14'>
-      <div className=' text-green font-semibold text-[20px] font-mono'>
-        <h1>Hi, my name is</h1>
-      </div>
-      <div className=' text-lightSlate text-[50px] font-semibold font-serif'>
-        <h2>Srishti Gautam.</h2>
-      </div>
-      <div className='text-slate text-[65px] font-bold font-fontSans leading-none'>
-        <h3>I build things for the web.</h3>
-      </div>
-      <p className='text-slate font-fontSans mt-6 text-[20px] max-w-[600px]'>
-        I’m a software engineer specializing in building intuitive and
-        user-friendly interfaces that provide an enjoyable and seamless user
-        experience.
-      </p>
-      <a
-        download=''
-        href='pdf/Srishti-gautam-resume.pdf'
-        className='text-green border border-green p-3 mt-5 rounded-md'
-      >
-        Download Resume<i className='uil uil-download-alt button__icon'></i>
-      </a>
-      {/* //Add intro 
+    <div className='flex gap-16 py-10 px-14'>
+      {/* py-10 px-14 */}
+      <div className='h-auto md:h-[40rem] rounded-md flex flex-col items-start  relative overflow-hidden '>
+        <div className=' text-green md:font-semibold text-[15px] font-mono md:text-[20px] font-medium'>
+          <h1>Hi, my name is</h1>
+        </div>
+        <div className=' text-lightSlate text-[30px] font-semibold font-serif md:text-[50px]'>
+          <h2>Srishti Gautam.</h2>
+        </div>
+        <div className='text-slate text-[40px] md:text-[65px] font-bold font-fontSans leading-none'>
+          <h3>I build things for the web.</h3>
+        </div>
+        <p className='text-slate font-fontSans mt-6 text-[20px] max-w-[600px]'>
+          I’m a software engineer specializing in building intuitive and
+          user-friendly interfaces that provide an enjoyable and seamless user
+          experience.
+        </p>
+        <a
+          download=''
+          href='pdf/Srishti-gautam-resume.pdf'
+          className='text-green border border-green p-3 mt-5 rounded-md'
+        >
+          Download Resume<i className='uil uil-download-alt button__icon'></i>
+        </a>
+        {/*  
       // make responsive */}
+      </div>
+      <div>
+        <Image
+          src={profile1}
+          alt='profile1'
+          className='h-64 w-64 rounded-md object-cover flex items-center mt-8'
+        />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,14 @@ import { IconLinkedin, IconGithub, IconTwitter } from "../asset/svgIcons";
 import Link from "next/link";
 
 function Navbar() {
-  const [active, setActive] = useState<string | null>(null);
-  // console.log(cn);
-
   return (
     //  top-10  mx-auto
     // border border-transparent dark:bg-black dark:border-white/[0.2] bg-white
-    <div className='fixed inset-x-0 w-full z-50   shadow-input flex justify-between items-center space-x-4 px-14 py-10 '>
+    <div className='inset-x-0 w-full z-50   shadow-input flex justify-between items-center space-x-4 px-14 py-10 dark:bg-navy'>
       <h1 className='text-[25px] font-semibold text-white'>Srishti Gautam</h1>
+      {/* {window.innerWidth < 730 ? ( */}
+      {/* <p>small</p>
+      ) : ( */}
       <div className='text-white flex items-center justify-around gap-7'>
         <Link href='https://www.linkedin.com/in/srishti-gautam-385861190/'>
           <div className='hover:text-purple-600'>
@@ -29,6 +29,7 @@ function Navbar() {
           </div>
         </Link>
       </div>
+      {/* )} */}
     </div>
   );
 }
